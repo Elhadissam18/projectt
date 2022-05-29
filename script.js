116f26d19ef3ed18874e84c5818a918040a42c9d
Elhadissam18 <89843077+Elhadissam18@users.noreply.github.com>
2022-05-29 21:31:17 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const panels = document.querySelectorAll(".panel");
+panels.forEach(panel => {
+  panel.addEventListener("click", () => {
+    remove();
+    panel.classList.add("active");
+  })
+})
+function remove() {
+
+    panels.forEach((panel) => {
+    panel.classList.remove("active");
+  })
+}
